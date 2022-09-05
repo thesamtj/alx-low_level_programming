18d185f5690442f31a7c1834b057c2db4a92e97d
Samuel Tijani <54986149+thesamtj@users.noreply.github.com>
2022-09-06 00:53:27 +0100

    Final commit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/winwin.so https://github.com/thesamtj/alx-low_level_programming/raw/master/0x18-dynamic_libraries/winwin.so
+wget -P -O /tmp https://github.com/thesamtj/alx-low_level_programming/raw/master/0x18-dynamic_libraries/winwin.so
 export LD_PRELOAD=/tmp/winwin.so
